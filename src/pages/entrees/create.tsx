c57305526db4a6bfdbe1f88314745af77ef6a506
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-07-18 20:11:38 -0400

    add supermarket feature

---
+++ src/pages/entrees/create.tsx
@@ -0,0 +1,11 @@
+import Form from "../../components/createEntreeForm";
+
+const Create = () => {
+    return (
+        <div>
+            <Form />
+        </div>
+    )
+}
+
+export default Create;
