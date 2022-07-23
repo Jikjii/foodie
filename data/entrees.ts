486890c6a4d2869834cba6b89094935000ac9edc
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-07-22 22:53:21 -0400

    entreea created

---
+++ data/entrees.ts
@@ -0,0 +1,29 @@
+export const entrees = [
+    {
+        id: "1",
+        name: "Spicy Miso Soup Ramen", 
+        description: "A delicous spicy ramen w/ a side of boiled egg",
+        rating: 5,
+        image: "https://dishingouthealth.com/wp-content/uploads/2022/01/SpicyMisoRamen_Square.jpg",    
+        category: "Noodles",
+        recipe: "https://ministryofcurry.com/spicy-miso-ramen/"
+    },
+    {
+        id: "2",
+        name: "Homemade Beef Pho", 
+        description: "A delicous spicy ramen w/ a side of boiled egg",
+        rating: 5,
+        image: "https://www.simplyrecipes.com/thmb/74yhAt6FmQ4nivy5P6vc2nCVKvE=/2000x1333/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__04__Beef-Pho-LEAD-2-afc6b6a9144947fb9d72070d7ea8c95c.jpg",    
+        category: "Noodles",
+        recipe: "https://theforkedspoon.com/pho-recipe/"
+    },
+    {
+        id: "3",
+        name: "Oven Pork Ribs", 
+        description: "A savory rack of porck ribs, rubbed in homemade bbq sauce",
+        rating: 5,
+        image: "https://www.onceuponachef.com/images/2022/06/baby-back-ribs-18.jpg",    
+        category: "",
+        recipe: "https://www.inspiredtaste.net/7179/sweet-and-spicy-oven-baked-ribs/"
+    }
+]
